1574f5b5785d2cce822a00e50d4b01f9d2392937
joy navi <joy.navi@gmail.com>
2018-05-16 14:30:32 -0400

    update

--- db/migrate/20180516145119_create_restaurants.rb
+++ db/migrate/20180516145119_create_restaurants.rb
@@ -1,9 +1,9 @@
 class CreateRestaurants < ActiveRecord::Migration[5.2]
   def change
     create_table :restaurants do |t|
-      t.string :reviews
       t.string :name
       t.string :address
+      t.string :phone_number
       t.string :category
 
       t.timestamps
